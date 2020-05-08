507f41d5b351407bbb65ae22d1d1623317835e58
Andrew Parrish <andrew.e.parrish@gmail.com>
2020-05-08 01:43:42 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,7 +15,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  return if age >= 13 || age <= 19 ? "You are a teenager!" : "You are not a teenager"
+
 }
 
 function switchAge(age) {
